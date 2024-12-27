90af364f91e9abdf325584aeebcbd89b42437b32
Esra <esraayaareb@gmail.com>
2024-12-27 22:29:16 +0300

    desktop design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,96 @@
+import React, { useState } from "react";
+import InputField from "./components/InputField";
+
 function App() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { firstName, lastName, email, password } = formData;
+    if (!firstName || !lastName || !email || !password) {
+      setError(true);
+    } else {
+      setError(false);
+      console.log("Form submitted:", formData);
+    }
+  };
   return (
     <>
-      <main>Test</main>
+      <main>
+        {/* Left  */}
+        <div>
+          <h1>Learn to code by watching others</h1>
+          <h2>
+            See how experienced developers solve problems in real-time. Watching
+            scripted tutorials is great, but understanding how developers think
+            is invaluable.
+          </h2>
+        </div>
+
+        {/* Right  */}
+        <div>
+          <h3>
+            <span>Try it free 7 days</span> then $20/mo. thereafter
+          </h3>
+
+          <div className="card">
+            <form onSubmit={handleSubmit}>
+              <InputField
+                type="text"
+                name="firstName"
+                placeholder="First Name"
+                value={formData.firstName}
+                onChange={handleChange}
+                error={error && !formData.firstName}
+              />
+              <InputField
+                type="text"
+                name="lastName"
+                placeholder="Last Name"
+                value={formData.lastName}
+                onChange={handleChange}
+                error={error && !formData.lastName}
+              />
+              <InputField
+                type="email"
+                name="email"
+                placeholder="Email Address"
+                value={formData.email}
+                onChange={handleChange}
+                error={error && !formData.email}
+              />
+              <InputField
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+                error={error && !formData.password}
+              />
+              <button type="submit">Claim your free trial</button>
+            </form>
+            <p className="terms_agreement">
+              <span>By clicking the button, you are agreeing to our </span>
+              <a href=""> Terms and Services</a>
+            </p>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
---
+++ src/components/InputField.jsx
@@ -0,0 +1,23 @@
+import IconError from "./icons/IconError";
+
+function InputField({ type, placeholder, value, onChange, error, name }) {
+  return (
+    <div className="input-wrapper">
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        name={name}
+        className={error ? "error" : ""}
+      />
+      {error && (
+        <span className="error-icon">
+          <IconError />
+        </span>
+      )}
+    </div>
+  );
+}
+
+export default InputField;
